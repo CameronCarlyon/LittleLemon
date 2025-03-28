36ecd3eb221c35d7b3d9a8d2192e88ebe81be483
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-03-28 15:00:53 +0000

    Footer and Header components updated

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import lemonLogo from '../assets/lemon.png';
+// import Promotion from './Promotion'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareFacebook, faSquareBluesky, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* <Promotion /> */}
+            <img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem'}} className='icon-white'/>
+            <ul>
+            <ul>
+                <li><Link to='/menu'>Menu</Link></li>
+                <li><Link to='/about-us'>About Us</Link></li>
+                <li><Link to='/our-restaurant'>Our Restaurant</Link></li>
+                <li><Link to='/contact-us'>Contact Us</Link></li>
+                <li><Link to='/faqs'>FAQs</Link></li>
+            </ul>
+            </ul>
+            <div className='social'>
+                <FontAwesomeIcon icon={faSquareFacebook} className='icon-social'/>
+                <FontAwesomeIcon icon={faSquareBluesky} className='icon-social'/>
+                <FontAwesomeIcon icon={faSquareInstagram} className='icon-social'/>
+            </div>
+            <h3 className='copyright'>Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a> | All rights reserved</h3>
+        </footer>
+    );
+};
+
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
+import lemonLogo from '../assets/lemon.png';
+
+const Header = () => {
+    return (
+        <header>
+            <Link to='/home'><img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem'}}/></Link>
+            
+            <ul>
+                <li><Link to='/menu'>Menu</Link></li>
+                <li><Link to='/about-us'>About Us</Link></li>
+                <li><Link to='/our-restaurant'>Our Restaurant</Link></li>
+                <li><Link to='/contact-us'>Contact Us</Link></li>
+                <li><Link to='/faqs'>FAQs</Link></li>
+            </ul>
+            <FontAwesomeIcon icon={faBasketShopping} />
+        </header>
+    );
+};
+
+export default Header;
