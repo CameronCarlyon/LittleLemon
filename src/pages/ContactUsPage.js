b8137b2c32932c499ed0957177b4b81ed359421f
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-04-02 16:50:20 +0100

    Update ContactUsPage.js

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
+import Promotion from '../components/Promotion.js';
 
 function ContactUsPage() {
   return (
     <div className="App">
       <Header />
-        <h1>Contact Us</h1>
-      <Footer />
-    </div>);
+        <div className="main-content">
+            <h1>Want to get in touch?</h1>
+            <div className="contact-info">
+                <div className="contact-details">
+                    <div>
+                        <h2>Address</h2>
+                        <p>1358 Chestnut St. <br />Chicago<br />IL 60680<br />
+                                    <p><a href='maps.google.com'>Get Directions</a></p>
+                                    </p>
+                    </div>
+                    <div>
+                        <h2>Phone Number</h2>
+                        <p>(312) 555-7890</p>
+                    </div>
+                    <div>
+                        <h2>Email Address</h2>
+                        <p>contact@littlelemon.com</p>
+                    </div>
+                </div>
+                <div className="contact-form">
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Subject" />
+                    <input type="text" placeholder="Message" />
+                    <button type="submit">Submit</button>
+                </div>
+            </div>
+                
+        </div>
+    <Promotion />
+    <Footer />
+    </div>
+  );
 }
 
 export default ContactUsPage;
