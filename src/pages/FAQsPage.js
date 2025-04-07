975c75c2787572ca82d6e9edd079e6079bf384bf
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-04-07 15:16:23 +0100

    Updated FAQs and added local API

--- src/pages/FAQsPage.js
+++ src/pages/FAQsPage.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import FAQ from '../components/FAQ.js';
 import Promotion from '../components/Promotion.js';
 
 function FAQsPage() {
-  return (
+return (
     <div>
-      <Header />
-        <div className="main-content">
-            <h1>Common Questions</h1>
-            <FAQ />
-        </div>
-        <Promotion />
-      <Footer />
+        <Header />
+            <div className="main-content">
+                    <h1>Frequently Asked Questions</h1>
+                    <p>Looking for answers? You may find them in the FAQs below.</p>
+                    <FAQ />
+                    <p>Still have questions? Feel free to <Link to={'/contact-us'}>reach out to us directly.</Link></p>
+            </div>
+            <Promotion />
+        <Footer />
     </div>);
 }
 
