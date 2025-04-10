df999c3eef72929157a358dc093ae248a99890f5
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-04-10 20:43:54 +0100

    Added reservation successful page

--- src/pages/ReservationSuccessfulPage.js
+++ src/pages/ReservationSuccessfulPage.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-import Header from '../components/Header.js';
-import ReservationCard from '../components/ReservationCard.js';
-import Promotion from '../components/Promotion.js';
-import Footer from '../components/Footer.js';
-
-let reservationCode = (Math.floor(Math.random() * 100000000));
+import Header from '../components/Header';
+import Promotion from '../components/Promotion';
+import Footer from '../components/Footer';
 
 const ReservationSuccessfulPage = () => {
+    const location = useLocation();
+    const reservationData = location.state?.reservationData;
+
     return (
         <div>
             <Header />
-            <div className='main-content'>
-                <h1>Reservation Successful</h1>
-                <p>We can't wait to welcome you!</p>
-                <ReservationCard />
-                <div>
-                    <p>Reservation Code</p>
-                    <h2>{reservationCode}</h2>    
+            <div className="reservation-details">
+                <h1>Booking Successful!</h1>
+                <h3>We Look Forward to Welcoming You!</h3>
+                <div className="reservation-card">
+                    <div>
+                        <p>Name</p>
+                        <p><strong>{reservationData?.fullName}</strong></p>
+                    </div>
+                    <div>
+                        <p>Date</p>
+                        <p><strong>{reservationData?.reservationDate}</strong></p>
+                    </div>
+                    <div>
+                        <p>Time</p>
+                        <p><strong>{reservationData?.reservationTime}</strong></p>
+                    </div>
+                    <div>
+                        <p>Guests</p>
+                        <p><strong>{reservationData?.guestCount}</strong></p>
+                    </div>
+                    {reservationData?.occasion !== 'none' && (
+                        <div>
+                            <p>Occasion</p>
+                            <p><strong>{reservationData?.occasion}</strong></p>
+                        </div>
+                        )}
+                    {reservationData?.specialRequests && (
+                        <div>
+                            <p>Special Requests</p>
+                            <p><strong>{reservationData?.specialRequests}</strong></p>
+                        </div>
+                    )}
+                    <div>
+                        <p>Contact</p>
+                        <p><strong>{reservationData?.emailAddress}</strong></p>
+                    </div>
                 </div>
-                <p>First time experiencing our restaurant? You can find out more information ahead of your visit <Link to='/our-restaurant'>here!</Link></p>
+                <p className="confirmation-message">
+                    A confirmation email has been sent to <strong><i>{reservationData?.emailAddress}</i></strong>
+                </p>
+                <p>First time visiting our restaurant? You can find out all you need to know <Link to={'/our-restaurant'}>here.</Link></p>
             </div>
             <Promotion />
             <Footer />
