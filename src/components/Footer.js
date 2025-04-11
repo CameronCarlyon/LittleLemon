5b379fc1a53c1a7f2191e64139d6675442c07322
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-04-11 18:13:27 +0100

    Responsive design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareFacebook, faSquareBluesky, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
+    const [isMobileView, setIsMobileView] = React.useState(window.innerWidth < 1248);
+
+    React.useEffect(() => {
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth < 1248);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <footer>
             <Link to='/home'><img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem'}} className='icon-white'/></Link>
             
-            <ul>
             <ul>
                 <li><Link to='/menu'>Menu</Link></li>
                 <li><Link to='/reservations'>Reservations</Link></li>
@@ -17,13 +27,22 @@ const Footer = () => {
                 <li><Link to='/contact-us'>Contact Us</Link></li>
                 <li><Link to='/faqs'>FAQs</Link></li>
             </ul>
-            </ul>
             <div className='social'>
                 <FontAwesomeIcon icon={faSquareFacebook} className='icon-social'/>
                 <FontAwesomeIcon icon={faSquareBluesky} className='icon-social'/>
                 <FontAwesomeIcon icon={faSquareInstagram} className='icon-social'/>
             </div>
-            <h3 className='copyright'>Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a> | All rights reserved</h3>
+            <h3 className='copyright'>
+                {isMobileView ? (
+                    <>
+                        Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a>
+                        <br />
+                        All rights reserved
+                    </>
+                ) : (
+                    <>Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a> | All rights reserved</>
+                )}
+            </h3>
         </footer>
     );
 };
