9e7fa70e6efb567f30a954950bae475ede1677e1
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-04-11 18:13:37 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
+import { faBasketShopping, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import lemonLogo from '../assets/lemon.png';
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header>
             <Link to='/home'><img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem'}}/></Link>
-            
-            <ul>
-                <li><Link to='/menu'>Menu</Link></li>
-                <li><Link to='/reservations'>Reservations</Link></li>
-                <li><Link to='/our-restaurant'>Our Restaurant</Link></li>
-                <li><Link to='/contact-us'>Contact Us</Link></li>
-                <li><Link to='/faqs'>FAQs</Link></li>
-            </ul>
-            <Link to='/shopping-cart'><FontAwesomeIcon className='icon' icon={faBasketShopping} /></Link>
+            <nav className={`main-nav ${isMenuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li><Link to='/menu' onClick={toggleMenu}>Menu</Link></li>
+                    {isMenuOpen && <span></span>}
+                    <li><Link to='/reservations' onClick={toggleMenu}>Reservations</Link></li>
+                    {isMenuOpen && <span></span>}
+                    <li><Link to='/our-restaurant' onClick={toggleMenu}>Our Restaurant</Link></li>
+                    {isMenuOpen && <span></span>}
+                    <li><Link to='/contact-us' onClick={toggleMenu}>Contact Us</Link></li>
+                    {isMenuOpen && <span></span>}
+                    <li><Link to='/faqs' onClick={toggleMenu}>FAQs</Link></li>
+                    {isMenuOpen && <span></span>}
+                    {isMenuOpen && <li><Link to='/shopping-basket'>Shopping Cart</Link></li>}
+                </ul>
+            </nav>
+            <Link to='/shopping-cart'><FontAwesomeIcon className='icon desktop-only' icon={faBasketShopping} /></Link>
+            <div 
+                className="burger-menu mobile-only" 
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+                aria-expanded={isMenuOpen}
+            >
+                <FontAwesomeIcon 
+                    className='icon' 
+                    icon={isMenuOpen ? faTimes : faBars} 
+                />
+            </div>
         </header>
     );
 };
