cf02327a0ed71c881002a5255de8b83810b0646a
Cameron Carlyon <cameroncarlyon@gmail.com>
2025-07-23 20:53:31 +0100

    Refactor Footer to use HyperlinkLabel and navigation array
    Replaces hardcoded navigation links in the Footer with a navigationItems array and the HyperlinkLabel component for consistency and maintainability. Also updates the copyright section to use HyperlinkLabel.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import lemonLogo from '../assets/lemon.png';
+import HyperlinkLabel from './HyperlinkLabel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareFacebook, faSquareBluesky, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
+    // Navigation items array
+    const navigationItems = [
+        { href: '/menu', text: 'Menu' },
+        { href: '/reservations', text: 'Reservations' },
+        { href: '/our-restaurant', text: 'Our Restaurant' },
+        { href: '/contact-us', text: 'Contact Us' },
+        { href: '/faqs', text: 'FAQs' }
+    ];
+
     const [isMobileView, setIsMobileView] = React.useState(window.innerWidth < 1248);
 
     React.useEffect(() => {
@@ -18,30 +28,25 @@ const Footer = () => {
 
     return (
         <footer>
-            <Link to='/home'><img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem'}} className='icon-white'/></Link>
-            
+            <Link to='/home'>
+                <img src={lemonLogo} alt='Little Lemon Logo' style={{ maxWidth: '3rem' }} className='icon-white' />
+            </Link>
             <ul>
-                <li><Link to='/menu'>Menu</Link></li>
-                <li><Link to='/reservations'>Reservations</Link></li>
-                <li><Link to='/our-restaurant'>Our Restaurant</Link></li>
-                <li><Link to='/contact-us'>Contact Us</Link></li>
-                <li><Link to='/faqs'>FAQs</Link></li>
+                {navigationItems.map((item) => (
+                    <li key={item.href}>
+                        <HyperlinkLabel href={item.href} text={item.text} />
+                    </li>
+                ))}
             </ul>
             <div className='social'>
-                <FontAwesomeIcon icon={faSquareFacebook} className='icon-social'/>
-                <FontAwesomeIcon icon={faSquareBluesky} className='icon-social'/>
-                <FontAwesomeIcon icon={faSquareInstagram} className='icon-social'/>
+                <FontAwesomeIcon icon={faSquareFacebook} className='icon-social' />
+                <FontAwesomeIcon icon={faSquareBluesky} className='icon-social' />
+                <FontAwesomeIcon icon={faSquareInstagram} className='icon-social' />
             </div>
             <h3 className='copyright'>
-                {isMobileView ? (
-                    <>
-                        Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a>
-                        <br />
-                        All rights reserved
-                    </>
-                ) : (
-                    <>Crafted by <a href="https://cameroncarlyon.com">Cameron Carlyon</a> | All rights reserved</>
-                )}
+                Crafted by <HyperlinkLabel href="https://cameroncarlyon.com" text="Cameron Carlyon" />
+                {isMobileView ? <br /> : ' | '}
+                All rights reserved
             </h3>
         </footer>
     );
