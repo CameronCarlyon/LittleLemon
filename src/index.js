c09a87a576c31aadb315f883f11d05532b7ae540
Cameron Carlyon <cameroncarlyon@gmail.com>
2026-03-13 19:14:56 +0000

    Add @fontsource and refactor font styles
    Add @fontsource/karla and @fontsource/markazi-text to package.json and import specific font-weight CSS in src/index.js. Remove local @font-face declarations from src/index.css and refactor styles to use CSS font-weight variables, add font-synthesis and font-optical-sizing, and adjust several selectors to use the new variables and minor spacing tweaks. package-lock.json updated accordingly.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/karla/200.css';
+import '@fontsource/karla/400.css';
+import '@fontsource/karla/500.css';
+import '@fontsource/karla/700.css';
+import '@fontsource/karla/800.css';
+import '@fontsource/markazi-text/400.css';
+import '@fontsource/markazi-text/500.css';
+import '@fontsource/markazi-text/600.css';
+import '@fontsource/markazi-text/700.css';
 import './index.css';
 import App from './App';
 
